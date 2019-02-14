3573861aadf473e550f03c1832454f57ee07fd0d
Antoine Panau <a.panau93@gmail.com>
2019-02-14 12:15:54 +0100

    Changement, configuration serveur

---
+++ Desktop/HETIC/JS/Chatbot/server.js
@@ -0,0 +1,27 @@
+/*
+Imports
+*/
+
+    const express = require("express");
+    const path = require("path");
+    const bodyParser = require("body-parser");
+    const ejs = require("ejs");
+
+//
+
+/*
+Configuration
+*/
+
+    const server = express();
+    const port = process.env.PORT;
+
+/*
+Démarrer le serveur
+*/
+
+    server.listen(port, () => {
+        console.log("Server is active on port ${port}");
+    });
+
+//
